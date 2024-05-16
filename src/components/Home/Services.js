7794db3561ccbbf39ea6dd4780ba6817aa09cbed
DevAKev <kevynpro7700@gmail.com>
2024-05-16 16:27:12 +0200

    Update services icons img & design cards

--- src/components/Home/Services.js
+++ src/components/Home/Services.js
@@ -1,9 +1,10 @@
 import { useTheme } from "../../hooks/ThemeContext";
 import ServiceCard from "../../components/Services/ServiceCard";
 import { ReactComponent as WebIcon } from "../../assets/images/icons/website.svg";
-import { ReactComponent as MobileIcon } from "../../assets/images/icons/mobiles.svg";
-import { ReactComponent as GameIcon } from "../../assets/images/icons/games.svg";
-import { ReactComponent as GraphicDesignIcon } from "../../assets/images/icons/graphic-design.svg";
+import MobileIcon from "../../assets/images/icons/mobiles.png";
+import GamesIcon from "../../assets/images/icons/games.png";
+import GraphicDesignIcon from "../../assets/images/icons/design.png";
+
 import SEOIcon from "../../assets/images/icons/seo.png";
 import ResponsiveIcon from "../../assets/images/icons/responsive.png";
 import ScrollAnimation from "react-animate-on-scroll";
@@ -15,11 +16,14 @@ const Services = () => {
 
   return (
     <div id="services" className="page-content">
-      <ScrollAnimation animateIn="animate__lightSpeedInLeft" animateOnce={true}>
+      <ScrollAnimation
+        animateIn="animate__bounceInUp"
+        animateOnce={true}
+        delay={3500}
+      >
         <h1 className="services-title">{titleKey}</h1>
-      </ScrollAnimation>
-      <ScrollAnimation animateIn="animate__bounceInUp" animateOnce={true}>
         <div className="services">
+          {/* Web development card */}
           <ServiceCard
             title={language === "en" ? "Web Development" : "Développement Web"}
             description={
@@ -30,6 +34,7 @@ const Services = () => {
             icon={<WebIcon />}
           />
 
+          {/* Mobile development card */}
           <ServiceCard
             title={
               language === "en" ? "Mobile Development" : "Développement Mobile"
@@ -39,21 +44,27 @@ const Services = () => {
                 ? "I design and develop mobile applications that are user-friendly, visually appealing, and easy to use."
                 : "Je conçois et développe des applications mobiles qui sont conviviales, visuellement attrayantes et faciles à utiliser."
             }
-            icon={<MobileIcon />}
+            icon={
+              <img className="mobile-icon" src={MobileIcon} alt="Mobile Icon" />
+            }
           />
 
+          {/* Games development card */}
           <ServiceCard
             title={
-              language === "en" ? "Game Development" : "Développement de Jeux"
+              language === "en" ? "Games Development" : "Développement de Jeux"
             }
             description={
               language === "en"
                 ? "I design and develop games that are visually appealing, user-friendly, and fun to play."
                 : "Je conçois et développe des jeux qui sont visuellement attrayants, conviviaux et amusants à jouer."
             }
-            icon={<GameIcon />}
+            icon={
+              <img className="games-icon" src={GamesIcon} alt="Games Icon" />
+            }
           />
 
+          {/* Graphic & design card */}
           <ServiceCard
             title={language === "en" ? "Graphic Design" : "Design Graphique"}
             description={
@@ -61,8 +72,16 @@ const Services = () => {
                 ? "I design visually appealing and user-friendly graphics and web designs."
                 : "Je conçois des graphiques et des web designs visuellement attrayants et conviviaux."
             }
-            icon={<GraphicDesignIcon />}
+            icon={
+              <img
+                className="design-icon"
+                src={GraphicDesignIcon}
+                alt="Graphic & design Icon"
+              />
+            }
           />
+
+          {/* SEO card */}
           <ServiceCard
             title={
               language === "en"
@@ -74,8 +93,10 @@ const Services = () => {
                 ? "I optimize websites for natural search engine ranking."
                 : "J'optimise les sites web pour le classement naturel dans les moteurs de recherche."
             }
-            icon={<img src={SEOIcon} alt="SEO Icon" />}
+            icon={<img className="seo-icon" src={SEOIcon} alt="SEO Icon" />}
           />
+
+          {/* Responsive design card */}
           <ServiceCard
             title={
               language === "en"
@@ -87,7 +108,13 @@ const Services = () => {
                 ? "I adapt websites for responsive design."
                 : "J'adapte les sites web pour le design réactif."
             }
-            icon={<img src={ResponsiveIcon} alt="Responsive design Icon" />}
+            icon={
+              <img
+                className="responsive-icon"
+                src={ResponsiveIcon}
+                alt="Responsive design Icon"
+              />
+            }
           />
         </div>
       </ScrollAnimation>

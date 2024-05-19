c26268b3ee79f4afb577f1ab4df00dc3ff5f50cf
DevAKev <kevynpro7700@gmail.com>
2024-05-19 19:21:30 +0200

    FixBUG responsive Header

--- src/components/Header/DesktopNavbar.js
+++ src/components/Header/DesktopNavbar.js
@@ -36,46 +36,59 @@ const DesktopNavBar = () => {
       </div>
       <div className="desktop_navbar_container">
         <ul>
-          <a
-            href="#home"
-            className={(nav) => (nav.isActive ? "nav-active" : "")}
-          >
-            <li>{language === "en" ? "HOME" : "ACCUEIL"}</li>
-          </a>
+          <li>
+            <a
+              href="#home"
+              className={(nav) => (nav.isActive ? "nav-active" : "")}
+            >
+              {language === "en" ? "HOME" : "ACCUEIL"}
+            </a>
+          </li>
 
-          <a
-            href="#services"
-            className={(nav) => (nav.isActive ? "nav-active" : "")}
-          >
-            <li>{language === "en" ? "SERVICES" : "SERVICES"}</li>
-          </a>
+          <li>
+            <a
+              href="#services"
+              className={(nav) => (nav.isActive ? "nav-active" : "")}
+            >
+              {language === "en" ? "SERVICES" : "SERVICES"}
+            </a>
+          </li>
 
-          {/* <a
-            href="#skills"
-            className={(nav) => (nav.isActive ? "nav-active" : "")}
-          >
-            <li>{language === "en" ? "SKILLS" : "COMPETENCES"}</li>
-          </a> */}
+          {/* <li>
+            <a
+              href="#skills"
+              className={(nav) => (nav.isActive ? "nav-active" : "")}
+            >
+              {language === "en" ? "SKILLS" : "COMPETENCES"}
+            </a>
+          </li> */}
 
-          <a
-            href="#projets"
-            className={(nav) => (nav.isActive ? "nav-active" : "")}
-          >
-            <li>{language === "en" ? "PROJECTS" : "PROJETS"}</li>
-          </a>
+          <li>
+            <a
+              href="#projets"
+              className={(nav) => (nav.isActive ? "nav-active" : "")}
+            >
+              {language === "en" ? "PROJECTS" : "PROJETS"}
+            </a>
+          </li>
 
-          <a
-            href="#about"
-            className={(nav) => (nav.isActive ? "nav-active" : "")}
-          >
-            <li>{language === "en" ? "ABOUT" : "À PROPOS"}</li>
-          </a>
-          <a
-            href="#contact"
-            className={(nav) => (nav.isActive ? "nav-active" : "")}
-          >
-            <li>{language === "en" ? "CONTACT" : "CONTACT"}</li>
-          </a>
+          <li>
+            <a
+              href="#about"
+              className={(nav) => (nav.isActive ? "nav-active" : "")}
+            >
+              {language === "en" ? "ABOUT" : "A PROPOS"}
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="#contact"
+              className={(nav) => (nav.isActive ? "nav-active" : "")}
+            >
+              {language === "en" ? "CONTACT" : "CONTACT"}
+            </a>
+          </li>
         </ul>
         {/* ### TO DO: AJOUTER LIENS RESEAUX SOCIAUX (VOIR https://www.onyedika.xyz/) ### */}
       </div>

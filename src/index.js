2f5a2b9a75d661e7784157c821e3cedbb1c1cdc8
DevAKev <kevynpro7700@gmail.com>
2024-05-19 15:08:18 +0200

    Add restrictions for Zoom & Right Click

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.scss";
+
+// Désactiver le clic droit
+document.addEventListener("contextmenu", (event) => event.preventDefault());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>

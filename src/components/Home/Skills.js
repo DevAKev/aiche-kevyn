4541d944c8dea519a6542f772214bba47e463d58
DevAKev <kevynpro7700@gmail.com>
2024-05-23 16:32:43 +0200

    Update Skills section & style services cards

--- src/components/Home/Skills.js
+++ src/components/Home/Skills.js
@@ -2,38 +2,89 @@ import { useTheme } from "../../hooks/ThemeContext";
 
 const Skills = () => {
   const { language } = useTheme();
-  const skillsKey = language === "en" ? "My Skills" : "Mes Compétences";
+  const skillsH1 = language === "en" ? "My Skills" : "Mes Compétences";
 
-  // List of skills
-  const skills = [
+  // Front-end skills
+  const frontEndSkills = [
     "html5",
     "css3",
     "sass",
     "javascript",
     "jquery",
     "react",
+    "nextjs",
+    "vuejs",
+    "bootstrap",
+    "tailwindcss",
+    "materialui",
+    // More front-end skills
+  ];
+
+  // Back-end skills
+  const backEndSkills = [
     "nodejs",
     "php",
-    // "mysql",
-    // "git",
-    // "github",
-    // "figma",
-    // "apache",
-    // More skills here
+    "mysql",
+    "python",
+    // More back-end skills
+  ];
+
+  // Tools skills
+  const tools = [
+    "git",
+    "webpack",
+    "github",
+    "figma",
+    "apache",
+    "nginx",
+    "bash",
+    "trello",
+    "slack",
+    "photoshop",
+    "illustrator",
+    "xd",
+    "vscode",
+    "filezilla",
+    // More tools
   ];
 
   return (
     <div id="skills" className="page-content">
-      <div className="about__skill-info">
-        <h3 className="my-skills-title">{skillsKey}</h3>
-        <ul className="about__skills">
-          {skills.map((skill, index) => (
-            <li key={index} className="about__skill">
-              <i className={`devicon-${skill}-plain skill-icon`}></i>
-              {skill} &nbsp;
-            </li>
-          ))}
-        </ul>
+      <div className="skills_container">
+        <h1 className="skills-title">{skillsH1}</h1>
+        <h2 className="skills-subtitles">Front-end :</h2>
+        <div className="skills-scroll">
+          <ul className="skills-ul">
+            {frontEndSkills.map((skill, index) => (
+              <li key={index} className="skills-li">
+                <i className={`devicon-${skill}-plain skills-icon`}></i>
+                {skill}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <h2 className="skills-subtitles">Back-end :</h2>
+        <div className="skills-scroll">
+          <ul className="skills-ul">
+            {backEndSkills.map((skill, index) => (
+              <li key={index} className="skills-li">
+                <i className={`devicon-${skill}-plain skills-icon`}></i>
+                {skill}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <h2 className="skills-subtitles">Outils :</h2>
+        <div className="skills-scroll">
+          <ul className="skills-ul">
+            {tools.map((tool, index) => (
+              <li key={index} className="skills-li">
+                <i className={`devicon-${tool}-plain skills-icon`}></i>
+                {tool}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );

496c3115a21969268f85acf749a321f0dad765c5
DevAKev <kevynpro7700@gmail.com>
2024-05-27 15:52:33 +0200

    Update services cards content

--- src/components/Home/Services.js
+++ src/components/Home/Services.js
@@ -33,16 +33,17 @@ const Services = () => {
                     in the universe of digital ?
                   </p>
                   <p>
-                    I create static and responsive websites tailored to your
-                    needs. A website is the showcase of your business,
-                    increasing your visibility and attracting new customers.
+                    I design custom websites that serve as a showcase digital
+                    technology to your business. Increase your visibility by and
+                    attract new customers with solutions web adapted to your
+                    needs.
                   </p>
                   <p>
                     Open the numeric door to your products or services, to make
                     it easy for potential customers to discover you. Whether you
-                    are an SME, a large company or an NGO, I can meet your
-                    specific needs. By choosing my services, you are investing
-                    in the growth of your business.
+                    are an SME, a large company or an NGO, I am here to meet
+                    your specific needs. By choosing my services, you are
+                    investing in the growth of your business.
                   </p>
                   <p>Ready for the take-off to online success ? 🚀</p>
                   <p>Contact me today to discuss your web development needs.</p>
@@ -54,17 +55,18 @@ const Services = () => {
                     entreprise dans l'univers du digital ?
                   </p>
                   <p>
-                    Je crée des sites web statiques et réactifs adaptés à vos
-                    besoins. Un site web est la vitrine de votre entreprise,
-                    augmentant votre visibilité et attirant de nouveaux clients.
+                    Je conçois des sites web sur mesure qui servent de vitrine
+                    digitale à votre entreprise. Augmentez votre visibilité en
+                    ligne et attirez de nouveaux clients grâce à des solutions
+                    web adaptées à vos besoins.
                   </p>
                   <p>
                     Ouvrez la porte numérique vers vos produits ou services,
                     pour permettre à vos clients potentiels de vous découvrir
                     facilement. Que vous soyez une PME, une grande entreprise ou
-                    une ONG, je peux répondre à vos besoins spécifiques. En
-                    choisissant mes services, vous investissez dans la
-                    croissance de votre entreprise.
+                    une ONG, je suis là pour répondre à vos besoins spécifiques.
+                    En choisissant mes services de développement web, vous
+                    investissez dans la croissance votre entreprise.
                   </p>
                   <p>Prêt à décoller vers le succès en ligne ? 🚀</p>
                   <p>
@@ -86,50 +88,48 @@ const Services = () => {
               language === "en" ? (
                 <div>
                   <p>
-                    Do you want to reach your future customers on any type of
-                    device ?
+                    Do you want to reach your future customers on all types of
+                    mobile devices?
                   </p>
                   <p>
                     Unlock the full potential of your business with custom
-                    mobile apps. I develop user-friendly and visually appealing
-                    mobile apps that are not only easy to use but also rank well
-                    in app store searches.
+                    mobile apps. I develop User-friendly and visually friendly
+                    mobile apps attractive, optimized to rank well in the app
+                    store searches.
                   </p>
                   <p>
-                    Experience the power of a mobile-first approach, I help you
-                    connect with your customers on a more personal level, right
-                    at your fingertips. Don't let your business be constrained
-                    by physical boundaries.
+                    Take a mobile-first approach to connect with your customers
+                    in a more personal way, right at your fingertips. Don't let
+                    your business be constrained by physical boundaries.
                   </p>
                   <p>
-                    Let me guide you through the stars and let's broaden your
-                    digital perspectives together. Contact me today and let's
-                    turn your vision into reality. 🌠
+                    Let yourself be guided through the stars and expand together
+                    your digital perspectives. Contact me now today and turn
+                    your vision into reality. 🌠
                   </p>
                 </div>
               ) : (
                 <div>
                   <p>
-                    Vous souhaitez atteindre vos futurs clients sur n'importe
-                    quel type d'appareil ?
+                    Vous souhaitez atteindre vos futurs clients sur tous les
+                    types d'appareils mobiles ?
                   </p>
                   <p>
                     Débloquez le plein potentiel de votre entreprise avec des
                     applications mobiles personnalisées. Je développe des
-                    applications mobiles conviviales et visuellement attrayantes
-                    qui sont non seulement faciles à utiliser et se classent
-                    également bien dans les recherches des magasins
-                    d'applications.
+                    applications mobiles conviviales et visuellement
+                    attrayantes, optimisées pour être bien classées dans les
+                    recherches des magasins d'applications.
                   </p>
                   <p>
-                    Expérimentez la puissance d'une approche mobile-first, je
-                    vous aide à vous connecter avec vos clients à un niveau plus
-                    personnel, directement à portée de main. Ne laissez pas
-                    votre entreprise être limitée par des frontières physiques.
+                    Adoptez une approche mobile-first pour vous connecter avec
+                    vos clients de manière plus personnelle, directement à
+                    portée de main. Ne laissez pas votre entreprise être limitée
+                    par des frontières physiques.
                   </p>
                   <p>
-                    Laissez moi vous guider à traver les étoiles et élargissons
-                    ensemble vos perspectives numérique. Contactez-moi
+                    Laissez vous guider à travers les étoiles et élargissons
+                    ensemble vos perspectives numérique. Contactez-moi dès
                     aujourd'hui et transformons votre vision en réalité. 🌠
                   </p>
                 </div>
@@ -157,24 +157,22 @@ const Services = () => {
                   <p>
                     Immerse your audience in the world of mobile gaming with
                     custom game development. Specializing in JavaScript, I
-                    design and develop mobile games that are not just visually
-                    appealing and user-friendly, but also packed with fun and
-                    excitement.
+                    design and develop mobile games full of fun and excitement.
                   </p>
                   <p>
                     Experience the thrill of engaging gameplay and impressive
                     graphics, right on your mobile device. Whether you're
                     looking to engage your audience with a casual game or
-                    captivate them with a more complex one, I've got you
-                    covered.
+                    captivate them with a more complex one, I am at your
+                    disposal.
                   </p>
                   <p>
-                    Let's transform your gaming idea into a mobile reality that
-                    keeps players coming back.
+                    Let's turn your game idea into a mobile reality that will
+                    keep players coming back again and again.
                   </p>
                   <p>
-                    Get in touch today to discuss your mobile game development
-                    needs.
+                    Contact me today to discuss your needs in mobile game
+                    development and embark together on this adventure ! 🤖
                   </p>
                 </div>
               ) : (
@@ -182,24 +180,24 @@ const Services = () => {
                   <p>
                     Plongez votre public dans le monde du jeu mobile avec le
                     développement de jeux personnalisés. Spécialisé en
-                    JavaScript, je conçois et développe des jeux mobiles qui
-                    sont non seulement visuellement attrayants et conviviaux,
-                    mais aussi remplis de fun et d'excitation.
+                    JavaScript, je conçois et développe des jeux mobiles bourrés
+                    de fun et d'excitation.
                   </p>
                   <p>
                     Vivez le frisson d'un gameplay engageant et de graphismes
                     impressionnants, directement sur votre appareil mobile. Que
                     vous cherchiez à engager votre public avec un jeu
                     occasionnel ou à le captiver avec un jeu plus complexe, je
-                    suis là pour vous.
+                    suis à votre disposition.
                   </p>
                   <p>
                     Transformons votre idée de jeu en une réalité mobile qui
-                    incite les joueurs à revenir.
+                    incitera les joueurs à revenir encore et encore.
                   </p>
                   <p>
                     Contactez-moi dès aujourd'hui pour discuter de vos besoins
-                    en matière de développement de jeux mobiles.
+                    en développement de jeux mobiles et embarquons ensemble dans
+                    cette aventure ! 🤖
                   </p>
                 </div>
               )
@@ -220,18 +218,18 @@ const Services = () => {
               language === "en" ? (
                 <div>
                   <p>
-                    Lacking inspiration or need a professional to assist you in
-                    exploring and researching visuals ?
+                    Lacking inspiration or need a professional to support you in
+                    the exploration and creation of unique visuals? 🎨
                   </p>
                   <p>
-                    Turn your ideas into impressive visuals. I create designs
-                    that are not only visually appealing but also user-friendly
-                    and effective in conveying your message.
+                    Turn your ideas into impressive visuals. I create original
+                    designs, effective in conveying your message and seduce your
+                    users.
                   </p>
                   <p>
-                    From assistance to mock-up, logos and brand identity and web
-                    design, I offer a wide range of design services. Design that
-                    can help your business stand out.
+                    From mock-up to logos, brand identity and web design, I
+                    offer a wide range of graphic design services to help your
+                    business stand out.
                   </p>
                   <p>
                     With an eye for detail and a creative mind, I strive to
@@ -243,20 +241,19 @@ const Services = () => {
                 <div>
                   <p>
                     En panne d'inspiration ou besoin d'un professionnel pour
-                    vous accompagner dans l'exploration et la recherche de
-                    visuels ?
+                    vous accompagner dans l'exploration et la création de
+                    visuels uniques ? 🎨
                   </p>
                   <p>
                     Transformez vos idées en graphisme impressionnants. Je crée
-                    des designs qui sont non seulement visuellement attrayants,
-                    mais aussi conviviaux et efficaces pour transmettre votre
-                    message.
+                    des designs originaux, efficaces pour transmettre votre
+                    message et séduire vos utilisateurs.
                   </p>
                   <p>
-                    De l'assistance au maquettage, en passant par les logos et
-                    l'identité de votre marque et au design web, je propose une
-                    large gamme de services de design graphique qui peuvent
-                    aider votre entreprise à se démarquer.
+                    Du maquettage aux logos, en passant par l'identité de votre
+                    marque et le design web, je propose une large gamme de
+                    services de design graphique pour aider votre entreprise à
+                    se démarquer.
                   </p>
                   <p>
                     Avec un œil attentif aux détails et un esprit créatif, je
@@ -286,9 +283,9 @@ const Services = () => {
               language === "en" ? (
                 <div>
                   <p>
-                    Increase your online presence and shine like the sun
-                    standing out in the darkness of space, with an effective SEO
-                    optimization strategy.
+                    Like the sun standing out from the other stars in the
+                    darkness of space, increase your online presence and shine
+                    with an effective SEO optimization strategy.
                   </p>
                   <p>
                     Specializing in enhancing natural search engine rankings, I
@@ -310,9 +307,10 @@ const Services = () => {
               ) : (
                 <div>
                   <p>
-                    Augmentez votre présence en ligne et brillez comme le soleil
-                    se distinguant dans l’obscurité de l’espace, grâce à une
-                    stratégie d’optimisation du référencement naturel efficace.
+                    Tel le soleil se distinguant des autres étoiles dans
+                    l’obscurité de l’espace, augmentez votre présence en ligne
+                    et brillez grâce à une stratégie d’optimisation du
+                    référencement naturel efficace.
                   </p>
                   <p>
                     Spécialisé dans l'amélioration du classement naturel dans
@@ -324,8 +322,8 @@ const Services = () => {
                   </p>
                   <p>
                     Ne laissez pas votre site web se perdre dans l'immensité des
-                    galaxies du numérique. Travaillons ensemble pour mettre
-                    votre entreprise au premier plan des résultats de recherche.
+                    galaxies numériques. Travaillons ensemble pour mettre votre
+                    entreprise au premier plan des résultats de recherche.
                   </p>
                   <p>
                     Contactez-moi dès aujourd'hui pour discuter de vos besoins
@@ -354,13 +352,13 @@ const Services = () => {
               language === "en" ? (
                 <div>
                   <p>
-                    Experience seamless browsing across all devices with my
+                    Experience smooth navigation on all devices with my
                     responsive design services.
                   </p>
                   <p>
-                    I specialize in adapting websites to ensure they look and
-                    function optimally on any screen size, from desktops to
-                    tablets and mobile phones.
+                    Specializing in website adaptation, I make sure they offer
+                    optimal appearance and functionality on all screen sizes,
+                    from desktops to tablets and mobile phones.
                   </p>
                   <p>
                     In the digital age, a responsive design isn't just an
@@ -369,21 +367,21 @@ const Services = () => {
                     your users will love, no matter what device they're on.
                   </p>
                   <p>
-                    Contact me today to discuss your responsive design needs and
-                    together we can launch your website into the digital space.
+                    Contact me here today to discuss your responsive design
+                    needs and ensure optimal navigation for your users. 📲
                   </p>
                 </div>
               ) : (
                 <div>
                   <p>
-                    Expérimentez une navigation sans couture sur tous les
-                    appareils avec mes services de design réactif.
+                    Expérimentez une navigation fluide sur tous les appareils
+                    grâce à mes services de design réactif.
                   </p>
                   <p>
-                    Je suis spécialisé dans l'adaptation des sites web pour
-                    garantir qu'ils ont une apparence et une fonctionnalité
-                    optimales sur n'importe quelle taille d'écran, des
-                    ordinateurs de bureau aux tablettes et téléphones mobiles.
+                    Spécialisé dans l'adaptation des sites web, je m'assure
+                    qu'ils offrent une apparence et une fonctionnalité optimales
+                    sur toutes les tailles d'écran, des ordinateurs de bureau
+                    aux tablettes et téléphones mobiles.
                   </p>
                   <p>
                     À l'ère du numérique, un design réactif n'est pas seulement
@@ -393,8 +391,9 @@ const Services = () => {
                     soit l'appareil qu'ils utilisent.
                   </p>
                   <p>
-                    Contactez-moi dès aujourd'hui pour discuter de vos besoins
-                    en matière de design réactif.
+                    Contactez-moi ici dès aujourd'hui pour discuter de vos
+                    besoins en design réactif et garantir une navigation
+                    optimale à vos utilisateurs. 📲
                   </p>
                 </div>
               )

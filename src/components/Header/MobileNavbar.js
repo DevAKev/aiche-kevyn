8c774615f3a4b251f6414162b9c29ac39912d9a1
DevAKev <kevynpro7700@gmail.com>
2024-06-11 19:40:56 +0200

    Add socials icons & links in Mobile Navbar

--- src/components/Header/MobileNavbar.js
+++ src/components/Header/MobileNavbar.js
@@ -1,18 +1,41 @@
 import { useState } from "react";
 import { useTheme } from "../../hooks/ThemeContext";
+// Theme Icons
 import darkModeIcon from "../../assets/images/theme/dark_emoji.png";
 import lightModeIcon from "../../assets/images/theme/light_emoji.png";
+// Language Icons
 import fr_flag from "../../assets/images/flags/fr_flag.png";
 import uk_flag from "../../assets/images/flags/uk_flag.png";
+// Socials Icons
+import { ReactComponent as FbIcon } from "../../assets/images/socials/facebook.svg";
+import { ReactComponent as TwitIcon } from "../../assets/images/socials/twitter.svg";
+import { ReactComponent as InstaIcon } from "../../assets/images/socials/instagram-icon.svg";
+import { ReactComponent as LinkIcon } from "../../assets/images/socials/linkedin-icon.svg";
+import { ReactComponent as GitIcon } from "../../assets/images/socials/github-icon.svg";
 
 // ### MOBILE NAVBAR ###
 const MobileNavBar = () => {
   const { theme, toggleTheme, language, toggleLanguage } = useTheme();
   const [showLinks, setShowLinks] = useState(false);
-
+  const [isHovered, setIsHovered] = useState(null);
   const handleShowLinks = () => {
     setShowLinks(!showLinks);
   };
+
+  const handleMouseOver = (index) => setIsHovered(index);
+  const handleMouseOut = () => setIsHovered(null);
+
+  const socialsLinks = [
+    { name: "Facebook", url: "https://www.facebook.com/", icon: FbIcon },
+    { name: "Twitter", url: "https://www.twitter.com/", icon: TwitIcon },
+    { name: "Instagram", url: "https://www.instagram.com/", icon: InstaIcon },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/kévyn-aiche",
+      icon: LinkIcon,
+    },
+    { name: "GitHub", url: "https://github.com/DevAKev", icon: GitIcon },
+  ];
 
   return (
     <nav className={`navbar ${showLinks ? "showNav" : "hideNav"}`}>
@@ -53,8 +76,6 @@ const MobileNavBar = () => {
           </a>
         </li>
 
-        {/* ### TO DO: AJOUTER LIENS RESEAUX SOCIAUX (VOIR https://www.onyedika.xyz/) ### */}
-
         {/* Icons for the themes modes */}
         <li className={`navbar__item slideInDown4`}>
           <div className="mobile_theme-toggle" onClick={toggleTheme}>
@@ -80,7 +101,35 @@ const MobileNavBar = () => {
             )}
           </div>
         </li>
+
+        {/* Social Media Icons */}
+        <p className="mobile_socials-title">
+          {language === "en" ? "Follow me" : "Suivez moi"}
+        </p>
+        <div className="navbar__social-links social-links">
+          {socialsLinks.map((link, index) => (
+            <li key={index} className={`navbar__item slideInDown${index + 5}`}>
+              <div className="mobile_socials-container">
+                <a
+                  href={link.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onMouseOver={() => handleMouseOver(index)}
+                  onMouseOut={handleMouseOut}
+                >
+                  <link.icon
+                    className={`social-icon ${
+                      isHovered === index ? theme : ""
+                    }`}
+                    alt={`${link.name} Icon`}
+                  />
+                </a>
+              </div>
+            </li>
+          ))}
+        </div>
       </ul>
+
       <button onClick={handleShowLinks} className="navbar__burger">
         <span className="burger__bar"></span>
       </button>

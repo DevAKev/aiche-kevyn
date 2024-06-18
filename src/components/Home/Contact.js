af72c4222eb7d7663b549158fefd1a3f4241364a
DevAKev <kevynpro7700@gmail.com>
2024-06-18 21:01:14 +0200

    Add socials link icon & put on hold form

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -1,13 +1,41 @@
 import { useTheme } from "../../hooks/ThemeContext";
 import videoBg from "../../assets/videos/background-video-contact.webm";
+import { ReactComponent as FbIcon } from "../../assets/images/socials/facebook-icon.svg";
+import { ReactComponent as TwitIcon } from "../../assets/images/socials/twitter-icon.svg";
+import { ReactComponent as InstaIcon } from "../../assets/images/socials/instagram-icon.svg";
+import { ReactComponent as LinkIcon } from "../../assets/images/socials/linkedin-icon.svg";
+import { ReactComponent as GitIcon } from "../../assets/images/socials/github-icon.svg";
+import { ReactComponent as GmailIcon } from "../../assets/images/socials/gmail-icon.svg";
 
 const Contact = () => {
   const language = useTheme();
   const contactTitle = language === "en" ? "Contact me" : "Me contacter";
   const contactText =
     language === "en"
-      ? " Do you have any questions ? Send me an email"
-      : "Vous avez des questions ? Envoyez-moi un email";
+      ? " Do you have any questions ?"
+      : "Vous avez des questions ?";
+
+  const socialsLinks = [
+    {
+      name: "Gmail",
+      url: "mailto:kevynpro7700@gmail.com",
+      icon: GmailIcon,
+    },
+    { name: "GitHub", url: "https://github.com/DevAKev", icon: GitIcon },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/kévyn-aiche",
+      icon: LinkIcon,
+    },
+    { name: "Facebook", url: "https://www.facebook.com/", icon: FbIcon },
+    { name: "Twitter", url: "https://www.twitter.com/", icon: TwitIcon },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/",
+      icon: InstaIcon,
+    },
+  ];
+
   return (
     <>
       <div id="contact" className="page-content">
@@ -17,7 +45,31 @@ const Contact = () => {
         <div className="contact-container">
           <h1 className="contact-title">{contactTitle}</h1>
           <p className="contact-text">{contactText}</p>
-          <form className="contact-form">
+          {/* Socials Media Icons */}
+          <nav className="socials-container">
+            {/* <p className="socials-title">
+              {language === "en" ? "Follow me" : "Suivez moi"}
+            </p> */}
+            <div className="bottom-socials-links">
+              {socialsLinks.map((link, index) => (
+                <a
+                  title={link.name}
+                  key={index}
+                  href={link.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <link.icon
+                    loading="lazy"
+                    className="bottom-socials-icon"
+                    alt={`${link.name} Icon`}
+                  />
+                </a>
+              ))}
+            </div>
+          </nav>
+          {/* Contact form */}
+          {/* <form className="contact-form">
             <input type="text" name="name" placeholder="Votre nom" required />
             <input
               type="email"
@@ -31,7 +83,7 @@ const Contact = () => {
               required
             ></textarea>
             <button type="submit">Envoyer</button>
-          </form>
+          </form> */}
         </div>
       </div>
     </>

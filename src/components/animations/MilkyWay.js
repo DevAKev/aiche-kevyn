271f8d0efbe890772122a5900deaa7a03a373cc2
DevAKev <kevynpro7700@gmail.com>
2024-06-28 17:04:31 +0200

    Add Loading animation before Start video

--- src/components/animations/MilkyWay.js
+++ src/components/animations/MilkyWay.js
@@ -1,11 +1,24 @@
+import { useState, useEffect } from "react";
 import BgVideo from "@assets/videos/start-background-video-milky-way.webm";
+
 const MilkyWay = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   // Error management
   const handleError = (event) => {
     console.error("Erreur lors du chargement de la vidéo", event);
   };
+
+  useEffect(() => {
+    const video = document.querySelector(".background-video");
+    video.oncanplaythrough = () => {
+      setIsLoaded(true);
+    };
+  }, []);
+
   return (
     <div className="video-container">
+      {!isLoaded && <div className="loading-spinner"></div>}
       <video
         autoPlay
         loop
@@ -13,7 +26,7 @@ const MilkyWay = () => {
         className="background-video"
         onError={handleError}
       >
-        <source src={BgVideo} type="video/webm" loading="lazy" />
+        <source src={BgVideo} type="video/webm" />
         Votre navigateur ne supporte pas cette vidéo.
       </video>
     </div>
